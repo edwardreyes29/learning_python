1bf5f4e63a2003eb480a767f6378c12af2143a21
Edward Reyes <reyes.edwardf@gmail.com>
2020-03-12 17:40:46 -0700

    first commit

---
+++ ch03/script1.py
@@ -0,0 +1,11 @@
+# A first Python script
+import sys          # Load a Library module
+print(sys.platform)
+print(2 ** 100)     # Raise 2 to a power 100
+x = 'Spam!'
+print(x * 8)        # String repetition
+
+# To print output to a file and save it
+# python script1.py > saveit.txt
+
+title = "The Meaning of Life"
---
+++ ch03/threenames.py
@@ -0,0 +1,6 @@
+# Define three attributes
+a = 'dead'
+b = 'parrot'
+c = 'sketch'
+print(a, b, c)
+
