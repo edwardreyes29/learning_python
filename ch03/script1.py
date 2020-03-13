c0b075d23272e5ed08ba63914489948beaba603f
Edward Reyes <reyes.edwardf@gmail.com>
2020-03-12 20:47:56 -0700

    ch 3 complete

--- ch03/script1.py
+++ ch03/script1.py
@@ -1,7 +1,7 @@
 # A first Python script
 import sys          # Load a Library module
 print(sys.platform)
-print(2 ** 100)     # Raise 2 to a power 100
+print(2 ** 32)     # Raise 2 to a power 100
 x = 'Spam!'
 print(x * 8)        # String repetition
 
